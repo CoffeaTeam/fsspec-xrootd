0c59624592eb1f6b5b578f1e5b3460721ef0bfb1
ScottDemarest <38106816+ScottDemarest@users.noreply.github.com>
2022-06-24 10:34:44 -0700

    File open feature (#4)
    * Read File Working
    
    Files can be read from local file system. Seek works as well. More thorough tests needed.
    
    * Linted with flake8
    
    * Typed everything
    
    * Mostly There
    
    * sign
    
    * Checks passed
    
    * Should pass now
    
    * Add fsspec dependency to library
    
    Cannot add XRootD dependency because it is not properly packaged :(
    
    * Revert "Merge branch 'file-system-ops-2' into File-Open-Feature"
    
    This reverts commit 2a27584691ff916dfe61c648660cb6a2c7e9eec6, reversing
    changes made to 33d710ee149f1a995d71a6434ffec2289027066d.
    
    * Add file open and read tests to CI
    
    * Remove windows and pypy CI tests
    
    xrootd is not available in conda for windows, and pypy not in conda
    
    * Passes pytest
    
    * lint
    
    * Implemented changes
    
    Co-authored-by: Nick Smith <nick.smith@cern.ch>

--- src/fsspec_xrootd/__init__.py
+++ src/fsspec_xrootd/__init__.py
@@ -8,5 +8,6 @@ fsspec-xrootd: xrootd implementation for fsspec
 from __future__ import annotations
 
 from ._version import version as __version__
+from .xrootd import XRootDFile, XRootDFileSystem
 
-__all__ = ("__version__",)
+__all__ = ("__version__", "XRootDFileSystem", "XRootDFile")
