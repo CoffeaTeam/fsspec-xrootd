d951d876dadbf249c2dd6f80515ecf5a03a38a74
ScottDemarest <38106816+ScottDemarest@users.noreply.github.com>
2022-08-11 19:04:11 -0400

    removed import xrootd test
    precommit doesnt like them

--- src/fsspec_xrootd/__init__.py
+++ src/fsspec_xrootd/__init__.py
@@ -11,14 +11,4 @@ from ._version import version as __version__
 from .xrootd import XRootDFile, XRootDFileSystem
 
 __all__ = ("__version__", "XRootDFileSystem", "XRootDFile")
-try:
-    import XRootD  # type: ignore[import]
-    import XRootD.client  # type: ignore[import]
 
-except ModuleNotFoundError:
-    raise ModuleNotFoundError(
-        """Install XRootD python bindings with:
-conda install -c conda-forge xrootd
-(or download from http://xrootd.org/dload.html and manually compile with """
-        """cmake; setting PYTHONPATH and LD_LIBRARY_PATH appropriately)."""
-    )
